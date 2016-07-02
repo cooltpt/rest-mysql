d3436135528b40b41398bfc3cdbe959375d29a6a
cooltpt <shaffer.david@outlook.com>
2016-07-01 22:04:47 -0600

    init..

---
+++ README.md
@@ -0,0 +1,8 @@
+# REST-MYSQL
+
+This is an example of a generic REST service wrapper using NodeJS/Express
+
+1. Edit the credentials, etc for the DB connection
+2. Setup the tables that you want to be REST-ified
+3. The server expects the 1st field of the table to be the PK
+4. Search for FIXME in the code for places to modify
---
+++ server.js
@@ -0,0 +1,92 @@
+var compress   = require("compression");
+var express    = require("express");
+var mysql      = require('mysql2/promise');
+var bodyParser = require("body-parser");
+var md5        = require('MD5');
+var logger     = require('winston');
+
+var rtw = require('./rest_table_wrap.js');
+
+var app = express();
+app.use(compress());
+
+
+/*
+// logger to IRC..
+logger.add(require('winston-irc'), {
+   host: 'irc.freenode.net',
+   nick: 'username', //FIXME
+   pass: 'userpass', //FIXME
+   channels: {
+     '#cstmnet': true
+  }
+});
+*/
+
+
+var serverCtx = {};
+serverCtx.env = process.env;
+logger.info("setting up server.. ");
+
+
+// db setup
+var sqlhost = process.env.SQLHOST || 'localhost'; //FIXME
+var sqldb   = process.env.SQLDB   || 'test'; //FIXME
+
+var pool = require('mysql2/promise').createPool({
+   connectionLimit: 100,
+   host: sqlhost, 
+   user: 'username', // FIXME
+   password: 'userpass', // FIXME
+   database: sqldb, 
+   debug: false
+});
+serverCtx.pool = pool;
+
+
+// setup app
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+// setup routers
+var router = express.Router();
+app.use('/api', router);
+
+// setup for test.test1 in MySQL - 1st field should be PK
+// add your tables here - FIXME
+rtw(serverCtx, router, pool, 'test1',    md5);
+rtw(serverCtx, router, pool, 'document', md5);
+rtw(serverCtx, router, pool, 'keyword',  md5);
+
+
+
+// handle everything else..
+app.use(function(req, res, next) {
+   var result = {};
+   result.hasError = true;
+   result.headers = req.headers;
+   result.url = req.url;
+   result.message = "Resource Not Found";
+   result.status = 404;
+   res.status = 404;
+   res.json(result);
+   logger.error(new Date() + ":" + JSON.stringify(result));
+});
+
+// static content setup - use nginx if possible
+app.use(express.static(__dirname + '/static'));
+logger.info("server has static content in " + __dirname + "/static");
+
+
+
+// start server
+if (module === require.main) {
+   var server = app.listen(process.env.PORT || 8080, function () {
+      var port = server.address().port;
+      logger.info('server is listening on port %s', port);
+   });
+}
+
+module.exports = app;
+
